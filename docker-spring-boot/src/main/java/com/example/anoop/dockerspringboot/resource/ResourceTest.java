955334476142ee72e2fbb679423325550ea574b7
Anoop Shukla <anoopshuklamcs@gmail.com>
2021-02-11 16:38:19 -0600

    Added Sample App for Docker

---
+++ docker-spring-boot/src/main/java/com/example/anoop/dockerspringboot/resource/ResourceTest.java
@@ -0,0 +1,18 @@
+package com.example.anoop.dockerspringboot.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class ResourceTest {
+
+	@GetMapping
+
+	public String hello() {
+		return "Hello Docker";
+	}
+
+}
